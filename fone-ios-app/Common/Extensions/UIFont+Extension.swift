337138ff383dc3a53bd7f2b224e1127ff9c36c15
g0getter <84381320+g0getter@users.noreply.github.com>
2023-08-13 00:06:20 +0900

    [FO-232] Style guide 적용 - Fonts (#6)
    * feat: Pretendard 폰트 파일 추가
    
    * feat: Pretendard 폰트 적용 extension 추가

---
+++ fone-ios-app/Common/Extensions/UIFont+Extension.swift
@@ -0,0 +1,62 @@
+//
+//  UIFont+Extension.swift
+//  fone-ios-app
+//
+//  Created by 여나경 on 2023/08/11.
+//
+
+import UIKit
+
+// 추후 폰트 종류가 추가되면 아래 case 추가
+enum FontType {
+    case pretendard
+    
+    var regular: String {
+        switch self {
+        case .pretendard: return "Pretendard-Regular"
+        }
+    }
+    var medium: String {
+        switch self {
+        case .pretendard: return "Pretendard-Medium"
+        }
+    }
+    var bold: String {
+        switch self {
+        case .pretendard: return "Pretendard-Bold"
+        }
+    }
+}
+
+extension UIFont {
+    
+    /// Bold 타입
+    class func font_b(_ size: CGFloat) -> UIFont {
+        let fontName = FontType.pretendard.bold
+        guard let font = UIFont(name: fontName, size: size) else {
+            return .systemFont(ofSize: size, weight: .bold)
+        }
+        return font
+    }
+    
+    /// Medium 타입
+    class func font_m(_ size: CGFloat) -> UIFont {
+        let fontName = FontType.pretendard.medium
+        guard let font = UIFont(name: fontName, size: size) else {
+            return .systemFont(ofSize: size, weight: .medium)
+        }
+
+        return font
+    }
+
+    /// Regular 타입
+    class func font_r(_ size: CGFloat) -> UIFont {
+        let fontName = FontType.pretendard.regular
+        guard let font = UIFont(name: fontName, size: size) else {
+            return .systemFont(ofSize: size, weight: .regular)
+        }
+
+        return font
+    }
+    
+}
