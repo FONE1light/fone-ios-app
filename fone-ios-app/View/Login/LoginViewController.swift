5086c89595b10656fe40b6307307e768f29021b7
Yukyung Huh <34676074+huhyukyung@users.noreply.github.com>
2023-08-31 18:22:47 +0900

    [FO-316][iOS] 로그인 UI (#16)
    * - IBInspectable 추가
    - shadowType, applyShadow 추가
    - 로그인 이미지 assets 추가
    
    * 로그인 화면 구성
    
    * forEach로 수정

--- fone-ios-app/View/Login/LoginViewController.swift
+++ fone-ios-app/View/Login/LoginViewController.swift
@@ -12,16 +12,74 @@ import NSObject_Rx
 class LoginViewController: UIViewController, ViewModelBindableType {
     var viewModel: LoginViewModel!
     
+   
+    @IBOutlet weak var kakaoLoginView: UIView!
+    @IBOutlet weak var naverLoginView: UIView!
+    @IBOutlet weak var googleLoginView: UIView!
+    @IBOutlet weak var appleLoginView: UIView!
+    
+    @IBOutlet weak var kakaoLoginButton: UIButton!
+    @IBOutlet weak var naverLoginButton: UIButton!
+    @IBOutlet weak var googleLoginButton: UIButton!
+    @IBOutlet weak var appleLoginButton: UIButton!
+    
+    @IBOutlet weak var emailLoginButton: UIButton!
     @IBOutlet weak var emailSignUpButton: UIButton!
+    @IBOutlet weak var questionButton: UIButton!
     
     func bindViewModel() {
+        kakaoLoginButton.rx.tap
+            .throttle(.milliseconds(500), scheduler: MainScheduler.instance)
+            .withUnretained(self)
+            .subscribe(onNext: { _ in
+            })
+            .disposed(by: rx.disposeBag)
+        
+        naverLoginButton.rx.tap
+            .throttle(.milliseconds(500), scheduler: MainScheduler.instance)
+            .withUnretained(self)
+            .subscribe(onNext: { _ in
+                
+            })
+            .disposed(by: rx.disposeBag)
+        
+        googleLoginButton.rx.tap
+            .throttle(.milliseconds(500), scheduler: MainScheduler.instance)
+            .withUnretained(self)
+            .subscribe(onNext: { _ in
+                
+            })
+            .disposed(by: rx.disposeBag)
+        
+        appleLoginButton.rx.tap
+            .throttle(.milliseconds(500), scheduler: MainScheduler.instance)
+            .withUnretained(self)
+            .subscribe(onNext: { _ in
+                
+            })
+            .disposed(by: rx.disposeBag)
+        
+        emailLoginButton.rx.tap
+            .throttle(.milliseconds(500), scheduler: MainScheduler.instance)
+            .withUnretained(self)
+            .subscribe(onNext: { _ in
+                
+            })
+            .disposed(by: rx.disposeBag)
+        
         emailSignUpButton.rx.tap
             .throttle(.milliseconds(500), scheduler: MainScheduler.instance)
             .withUnretained(self)
             .subscribe(onNext: { _ in
-                let findIDPasswordViewModel = FindIDPasswordViewModel(sceneCoordinator: self.viewModel.sceneCoordinator)
-                let findIDPasswordScene = Scene.findIDPassword(findIDPasswordViewModel)
-                self.viewModel.sceneCoordinator.transition(to: findIDPasswordScene, using: .push, animated: true)
+                
+            })
+            .disposed(by: rx.disposeBag)
+        
+        questionButton.rx.tap
+            .throttle(.milliseconds(500), scheduler: MainScheduler.instance)
+            .withUnretained(self)
+            .subscribe(onNext: { _ in
+                
             })
             .disposed(by: rx.disposeBag)
         
@@ -30,5 +88,8 @@ class LoginViewController: UIViewController, ViewModelBindableType {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        [ kakaoLoginView, naverLoginView, googleLoginView, appleLoginView ].forEach {
+            $0.applyShadow(shadowType: .shadowIt2)
+        }
     }
 }
