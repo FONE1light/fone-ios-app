31444d5ec21c51f60848ea9de3d05fc7f544f83e
huhyukyung@gmail.com <huhyukyung@gmail.com>
2023-09-07 22:16:15 +0900

    feat: 아이디 비밀번호 탭 선택 애니메이션

--- fone-ios-app/View/FindIDPassword/FindIDPasswordViewController.swift
+++ fone-ios-app/View/FindIDPassword/FindIDPasswordViewController.swift
@@ -11,14 +11,42 @@ class FindIDPasswordViewController: UIViewController, ViewModelBindableType {
     var viewModel: FindIDPasswordViewModel!
     
     @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var selectedTabLeadingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var idTabButton: UIButton!
+    @IBOutlet weak var passwordTabButton: UIButton!
+    
     
     func bindViewModel() {
         backButton.rx.tap
             .withUnretained(self)
             .subscribe(onNext: { _ in
                 self.navigationController?.popViewController(animated: true)
-            })
-            .disposed(by: rx.disposeBag)
+            }).disposed(by: rx.disposeBag)
+        
+        idTabButton.rx.tap
+            .withUnretained(self)
+            .subscribe(onNext: { _ in
+                self.idTabButton.setTitleColor(.red_CE0B39, for: .normal)
+                self.passwordTabButton.setTitleColor(.gray_9E9E9E, for: .normal)
+                self.view.layoutIfNeeded()
+                UIView.animate(withDuration: 0.3, delay: 0, animations: {
+                    self.selectedTabLeadingConstraint.constant = 0
+                    self.view.layoutIfNeeded()
+                })
+            }).disposed(by: rx.disposeBag)
+        
+        passwordTabButton.rx.tap
+            .withUnretained(self)
+            .subscribe(onNext: { _ in
+                self.idTabButton.setTitleColor(.gray_9E9E9E, for: .normal)
+                self.passwordTabButton.setTitleColor(.red_CE0B39, for: .normal)
+                self.view.layoutIfNeeded()
+                UIView.animate(withDuration: 0.3, delay: 0, animations: {
+                    self.selectedTabLeadingConstraint.constant = self.idTabButton.frame.width
+                    self.view.layoutIfNeeded()
+                })
+            }).disposed(by: rx.disposeBag)
+        
     }
     
     override func viewDidLoad() {
