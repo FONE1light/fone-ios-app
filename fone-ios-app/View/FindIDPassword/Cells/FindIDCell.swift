d774f3ff55a4f82a6d0dea536f02656d1cb12251
huhyukyung@gmail.com <huhyukyung@gmail.com>
2023-09-10 18:34:43 +0900

    feat: 아이디/비밀번호 찾기 화면 UI - 스와이프하여 화면 전환될 수 있게 처리

---
+++ fone-ios-app/View/FindIDPassword/Cells/FindIDCell.swift
@@ -0,0 +1,16 @@
+//
+//  FindIDCell.swift
+//  fone-ios-app
+//
+//  Created by Yukyung Huh on 2023/09/10.
+//
+
+import UIKit
+
+class FindIDCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
