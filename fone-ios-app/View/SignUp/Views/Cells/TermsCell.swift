2ead5d93b784eb397be53ab3a1dca5d87b55f860
나경 <nagyeong.yeo1023@gmail.com>
2023-09-10 17:21:58 -0700

    feat: TableView extension 추가

---
+++ fone-ios-app/View/SignUp/Views/Cells/TermsCell.swift
@@ -0,0 +1,23 @@
+//
+//  TermsTableViewCell.swift
+//  fone-ios-app
+//
+//  Created by 여나경 on 2023/09/10.
+//
+
+import UIKit
+
+class TermsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
