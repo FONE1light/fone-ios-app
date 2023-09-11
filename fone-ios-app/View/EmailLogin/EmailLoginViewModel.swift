5667e8583b8de77a39ce78d900d6deece51bfd9c
Yukyung Huh <34676074+huhyukyung@users.noreply.github.com>
2023-09-11 14:53:54 +0900

    [FO-416][iOS] 로그인 실패 시 처리 수정 (#25)
    * feat: 로그인 실패 시 처리
    
    * feat: 에러메시지 토스트 삭제

--- fone-ios-app/View/EmailLogin/EmailLoginViewModel.swift
+++ fone-ios-app/View/EmailLogin/EmailLoginViewModel.swift
@@ -5,13 +5,14 @@
 //  Created by Yukyung Huh on 2023/09/02.
 //
 
-import Foundation
+import UIKit
 import RxSwift
 
 class EmailLoginViewModel: CommonViewModel {
     var disposeBag = DisposeBag()
     var emailTextSubject = BehaviorSubject<String>(value: "")
     var passwordIsEmptySubject = BehaviorSubject<Bool>(value: true)
+    var showLoginErrorAlertSubject = PublishSubject<Void>()
     var isKeyboardShowing = false
     
     lazy var emailIsValidSubject: Observable<Bool> = {
@@ -45,9 +46,11 @@ class EmailLoginViewModel: CommonViewModel {
                     // TODO: 로그인 성공 후 처리
                     
                     UserDefaults.standard.set(response.data?.token.accessToken, forKey: "accessToken")
+                    UserDefaults.standard.set(response.data?.token.refreshToken, forKey: "refreshToken")
                     
+                    response.data?.token.accessToken.toast(isKeyboardShowing: self.isKeyboardShowing)
                 } else {
-                    response.message.toast(isKeyboardShowing: self.isKeyboardShowing)
+                    self.showLoginErrorAlertSubject.onNext(())
                 }
             }).disposed(by: disposeBag)
     }
