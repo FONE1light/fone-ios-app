00cd65f93c2d5f4b1d04d7e7755eaee4f015395f
huhyukyung@gmail.com <huhyukyung@gmail.com>
2023-09-17 20:56:03 +0900

    feat: 메인 배너 무한 스크롤, 오토 스크롤

--- fone-ios-app/View/Home/Modules/MainBanner/MainBannerModule.swift
+++ fone-ios-app/View/Home/Modules/MainBanner/MainBannerModule.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 class MainBannerModule: UICollectionViewCell {
+    let bannerImageList: [UIImage] = [#imageLiteral(resourceName: "banner1"), #imageLiteral(resourceName: "banner2"), #imageLiteral(resourceName: "banner3"), #imageLiteral(resourceName: "banner4"), #imageLiteral(resourceName: "banner5"), #imageLiteral(resourceName: "banner6")]
+    private var timer: Timer?
+    
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var pageCountLabel: UILabel!
     
@@ -17,17 +20,57 @@ class MainBannerModule: UICollectionViewCell {
         collectionView.dataSource = self
         collectionView.delegate = self
         collectionView.register(MainBannerCell.self)
+        
+        layoutIfNeeded()
+        let index = bannerImageList.count
+        let indexPath = IndexPath(item: index, section: 0)
+        collectionView.scrollToItem(at: indexPath,
+                                    at: .centeredHorizontally,
+                                    animated: false)
+        
+        startTimer()
+    }
+    
+    private func stopTimer() {
+        timer?.invalidate()
+    }
+    
+    private func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 3,
+                                     target: self,
+                                     selector: #selector(scrollToNext),
+                                     userInfo: nil,
+                                     repeats: true)
+    }
+    
+    @objc func scrollToNext() {
+        var item = visibleCellIndexPath().item
+        if item == bannerImageList.count * 3 - 1 {
+            collectionView.scrollToItem(at: IndexPath(item: bannerImageList.count * 2 - 1, section: 0),
+                                        at: .centeredHorizontally,
+                                        animated: false)
+            item = bannerImageList.count * 2 - 1
+        }
+        
+        item += 1
+        collectionView.scrollToItem(at: IndexPath(item: item, section: 0),
+                                    at: .centeredHorizontally,
+                                    animated: true)
+        let currentPage: Int = item % bannerImageList.count + 1
+        updatePageCount(currentPage)
     }
 }
 
 extension MainBannerModule: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 6
+        return bannerImageList.count * 3
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let bannerImage = bannerImageList[indexPath.item % bannerImageList.count]
         let cell = collectionView.dequeueReusableCell(forIndexPath: indexPath) as MainBannerCell
-        cell.configure(index: indexPath.row)
+        cell.imageView.image = bannerImage
+        //        cell.configure(index: indexPath.row)
         return cell
     }
 }
@@ -42,8 +85,35 @@ extension MainBannerModule: UICollectionViewDelegateFlowLayout {
 }
 
 extension MainBannerModule: UIScrollViewDelegate {
+    private func visibleCellIndexPath() -> IndexPath {
+        return collectionView.indexPathsForVisibleItems[0]
+    }
+    
+    private func updatePageCount(_ currentPage: Int) {
+        pageCountLabel.text = "\(currentPage) / \(bannerImageList.count)"
+    }
+    
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        self.stopTimer()
+        self.startTimer()
+        
+        var item = visibleCellIndexPath().item
+        
+        if item == bannerImageList.count * 3 - 2 {
+            item = bannerImageList.count * 2
+        } else if item == 1 {
+            item = bannerImageList.count + 1
+        }
+        collectionView.isPagingEnabled = false
+        collectionView.scrollToItem(at: IndexPath(item: item, section: 0),
+                                    at: .centeredHorizontally,
+                                    animated: false)
+        collectionView.isPagingEnabled = true
+    }
+    
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-        let currentPage = Int(scrollView.contentOffset.x / scrollView.frame.width) + 1
-        pageCountLabel.text = "\(currentPage) / 6"
+        let item = visibleCellIndexPath().item
+        let currentPage: Int = item % bannerImageList.count + 1
+        updatePageCount(currentPage)
     }
 }
