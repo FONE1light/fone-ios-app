cb818f053efbb413a20c87f7544dd1ece5257a38
sinkyoungdeok <tkagmd1@naver.com>
2023-09-24 00:36:14 +0900

    [FO-437] 홈 - 배너

---
+++ fone-ios-app/View/Home/HomeViewController.swift
@@ -0,0 +1,118 @@
+//
+//  HomeViewController.swift
+//  fone-ios-app
+//
+//  Created by Yukyung Huh on 2023/09/11.
+//
+
+import UIKit
+
+enum ModuleSection: Int, CaseIterable {
+    case mainBanner = 0
+    case jobOpening
+    case competition
+    case profile
+}
+
+class HomeViewController: UIViewController, ViewModelBindableType {
+    var viewModel: HomeViewModel!
+    var hasViewModel = false
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setNavigationBar()
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(MainBannerModule.self)
+        collectionView.register(JobOpeningModule.self)
+        collectionView.register(CompetitionModule.self)
+        collectionView.register(ProfileModule.self)
+    }
+    
+    func bindViewModel() {
+        
+    }
+    
+    private func setNavigationBar() {
+        navigationController?.navigationBar.isTranslucent = false
+        navigationController?.hidesBarsOnSwipe = true
+        
+        let navigationBarAppearance = UINavigationBarAppearance()
+        navigationBarAppearance.shadowColor = nil
+        navigationBarAppearance.backgroundColor = .white
+        
+        navigationController?.navigationBar.standardAppearance = navigationBarAppearance
+        navigationController?.navigationBar.scrollEdgeAppearance = navigationBarAppearance
+        
+        self.navigationItem.leftBarButtonItem = NavigationLeftBarButtonItem(type: .home)
+        self.navigationItem.rightBarButtonItem = NavigationRightBarButtonItem(type: .notification, viewController: self)
+    }
+}
+
+extension HomeViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        var cell = UICollectionViewCell()
+        
+        switch indexPath.section {
+        case ModuleSection.mainBanner.rawValue:
+            cell = collectionView.dequeueReusableCell(forIndexPath: indexPath) as MainBannerModule
+        case ModuleSection.jobOpening.rawValue:
+            cell = collectionView.dequeueReusableCell(forIndexPath: indexPath) as JobOpeningModule
+        case ModuleSection.competition.rawValue:
+            cell = collectionView.dequeueReusableCell(forIndexPath: indexPath) as CompetitionModule
+        case ModuleSection.profile.rawValue:
+            cell = collectionView.dequeueReusableCell(forIndexPath: indexPath) as ProfileModule
+        default:
+            break
+        }
+        
+        return cell
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return ModuleSection.allCases.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+}
+
+extension HomeViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width: Double = UIScreen.main.bounds.width
+        var height: Double = 250
+        switch indexPath.section {
+        case ModuleSection.mainBanner.rawValue:
+            height = 250
+        case ModuleSection.jobOpening.rawValue:
+            height = 214
+        case ModuleSection.competition.rawValue:
+            height = 259
+        case ModuleSection.profile.rawValue:
+            height = 249
+        default:
+            height = 0
+        }
+        
+        return CGSize(width: width, height: height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        switch section {
+        case ModuleSection.mainBanner.rawValue:
+            return UIEdgeInsets(top: 0, left: 0, bottom: 32, right: 0)
+        case ModuleSection.jobOpening.rawValue:
+            return UIEdgeInsets(top: 0, left: 0, bottom: 40, right: 0)
+        case ModuleSection.competition.rawValue:
+            return UIEdgeInsets(top: 0, left: 0, bottom: 40, right: 0)
+        case ModuleSection.profile.rawValue:
+            return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        default: return  .zero
+        }
+    }
+}
