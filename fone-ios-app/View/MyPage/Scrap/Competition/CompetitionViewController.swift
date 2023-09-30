c709156aa86d880f517c5b19868ef41da566fed9
나경 <nagyeong.yeo1023@gmail.com>
2023-09-29 23:02:17 -0400

    feat: 공모전 셀 추가, TabViewController에 포함되는 공모전 view controller 추가

---
+++ fone-ios-app/View/MyPage/Scrap/Competition/CompetitionViewController.swift
@@ -0,0 +1,66 @@
+//
+//  CompetitionViewController.swift
+//  fone-ios-app
+//
+//  Created by 여나경 on 2023/09/26.
+//
+
+import UIKit
+import SnapKit
+
+class CompetitionViewController: UIViewController, ViewModelBindableType {
+    
+    var viewModel: CompetitionViewModel!
+    
+    private lazy var tableView = UITableView().then {
+        $0.showsVerticalScrollIndicator = false
+        $0.separatorStyle = .none
+//        $0.delegate = self
+        $0.dataSource = self
+        $0.register(with: CompetitionCell.self)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupUI()
+        setupConstraints()
+    }
+    
+    func bindViewModel() {
+        
+    }
+    
+    
+    private func setupUI() {
+        [tableView]
+            .forEach { view.addSubview($0) }
+    }
+    
+    private func setupConstraints() {
+        tableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+}
+
+extension CompetitionViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(for: indexPath) as CompetitionCell
+        cell.configure(
+            title: "제목 예 2022 베리어프리 콘텐츠 공모전 제목 예 2022 베리어프리 콘텐츠 공모전",
+            coorporation: "방송통신 위원회",
+            leftDays: "D-15",
+            viewCount: 3222
+        )
+        
+        return cell
+    }
+    
+    
+}
