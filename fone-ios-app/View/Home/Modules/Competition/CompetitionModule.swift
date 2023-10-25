8273f8a536508a49e640d4cc84ca726680df9681
huhyukyung@gmail.com <huhyukyung@gmail.com>
2023-10-25 22:07:27 +0900

    feat: 공모전 불러오기에 실패했을 경우의 화면 추가

--- fone-ios-app/View/Home/Modules/Competition/CompetitionModule.swift
+++ fone-ios-app/View/Home/Modules/Competition/CompetitionModule.swift
@@ -8,10 +8,28 @@
 import UIKit
 
 class CompetitionModule: UICollectionViewCell {
+    var competitionInfo: ModuleInfo?
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var errorView: UIView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
+    func setModuelInfo(info: ModuleInfo?) {
+        titleLabel.text = info?.title ?? "인기 영화제"
+        self.competitionInfo = info
+        setModule()
+    }
+    
+    private func setModule() {
+        guard competitionInfo != nil else {
+            errorView.isHidden = false
+            collectionView.isHidden = true
+            return
+        }
+        collectionView.reloadData()
+    }
 }
