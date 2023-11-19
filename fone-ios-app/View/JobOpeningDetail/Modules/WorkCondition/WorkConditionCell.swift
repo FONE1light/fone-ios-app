4e12e70556beeb6fb587c026410ff161dca18e48
huhyukyung@gmail.com <huhyukyung@gmail.com>
2023-11-19 22:01:27 +0900

    feat: WorkConditionCell IBOutlet 작업

--- fone-ios-app/View/JobOpeningDetail/Modules/WorkCondition/WorkConditionCell.swift
+++ fone-ios-app/View/JobOpeningDetail/Modules/WorkCondition/WorkConditionCell.swift
@@ -8,10 +8,14 @@
 import UIKit
 
 class WorkConditionCell: UICollectionViewCell {
-
+    @IBOutlet weak var salaryTypeLabel: UILabel!
+    @IBOutlet weak var salaryLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var periodLabel: UILabel!
+    @IBOutlet weak var workDaysLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
 }
