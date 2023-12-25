7b042adcdf819ea205b2df4ac091f962c0d42e06
huhyukyung@gmail.com <huhyukyung@gmail.com>
2023-12-25 21:37:14 +0900

    feat: 필터 헤더

--- fone-ios-app/View/Filter/FilterViewController.swift
+++ fone-ios-app/View/Filter/FilterViewController.swift
@@ -9,16 +9,27 @@ import UIKit
 
 class FilterViewController: UIViewController, ViewModelBindableType {
     var viewModel: FilterViewModel!
-
+    
+    @IBOutlet weak var closeButton: UIButton!
     @IBOutlet weak var confirmButton: CustomButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         confirmButton.xibInit("보러가기", type: .bottom)
     }
     
     func bindViewModel() {
+        closeButton.rx.tap
+            .withUnretained(self)
+            .bind { owner, _ in
+                owner.viewModel.sceneCoordinator.close(animated: true)
+            }.disposed(by: rx.disposeBag)
         
+        confirmButton.rx.tap
+            .withUnretained(self)
+            .bind { owner, _ in
+                owner.viewModel.sceneCoordinator.close(animated: true)
+            }.disposed(by: rx.disposeBag)
     }
 }
