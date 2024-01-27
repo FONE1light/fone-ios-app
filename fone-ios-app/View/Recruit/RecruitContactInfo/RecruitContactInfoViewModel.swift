c94c23b27d7954cd2a4cfcbb0d36f267c3042b8d
huhyukyung@gmail.com <huhyukyung@gmail.com>
2024-01-27 22:10:59 +0900

    feat: 모집 등록 성공시 팝업, 스태프 모집

--- fone-ios-app/View/Recruit/RecruitContactInfo/RecruitContactInfoViewModel.swift
+++ fone-ios-app/View/Recruit/RecruitContactInfo/RecruitContactInfoViewModel.swift
@@ -5,7 +5,7 @@
 //  Created by Yukyung Huh on 12/13/23.
 //
 
-import Foundation
+import UIKit
 import RxSwift
 
 struct RecruitContactInfo {
@@ -40,9 +40,43 @@ final class RecruitContactInfoViewModel: CommonViewModel {
             .withUnretained(self)
             .subscribe(onNext: { owner, response in
                 print(response)
-                
+                owner.showSuccessPopUp()
             }, onError: { error in
                 print(error)
             }).disposed(by: disposeBag)
+    }
+}
+
+// MARK: - 화면 전환 관련
+extension RecruitContactInfoViewModel {
+    private func showSuccessPopUp() {
+        let message = "등록에 성공하였습니다"
+        let alert = UIAlertController.createOneButtonPopup(title: message) { [weak self] _ in
+            guard let self = self else { return }
+            self.popToJobOpening()
+        }
+        guard let currentVC = (sceneCoordinator as? SceneCoordinator)?.currentVC else { return }
+        currentVC.present(alert, animated: true) {
+            alert.view.superview?.isUserInteractionEnabled = true
+            alert.view.superview?.addGestureRecognizer(
+                UITapGestureRecognizer(
+                    target: self,
+                    action: #selector(self.popToJobOpening)
+                )
+            )
+        }
+    }
+    
+    @objc private func popToJobOpening() {
+        guard let currentVC = (sceneCoordinator as? SceneCoordinator)?.currentVC,
+              let navigationController = currentVC.navigationController else { return }
+        
+        currentVC.dismiss(animated: false) {
+            for vc in navigationController.viewControllers {
+                if let jobOpeningHuntingVC = vc as? JobOpeningHuntingViewController {
+                    navigationController.popToViewController(jobOpeningHuntingVC, animated: true)
+                }
+            }
+        }
     }
 }
