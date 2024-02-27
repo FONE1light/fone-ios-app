f0afc902e09fd362aa920fcdf6f827ea4ad94043
Yukyung Huh <huhyukyung@gmail.com>
2024-02-27 16:40:46 +0900

    [FO-939]연락 방법 선택 바텀시트 (#159)
    * feat: 연락방법 바텀시트
    
    * feat: 로그인 에러 토스트

--- fone-ios-app/View/Recruit/RecruitContactLinkInfo/RecruitContactLinkInfoViewModel.swift
+++ fone-ios-app/View/Recruit/RecruitContactLinkInfo/RecruitContactLinkInfoViewModel.swift
@@ -6,6 +6,8 @@
 //
 
 import Foundation
+import RxSwift
+import RxRelay
 
 struct RecruitContactLinkInfo: Codable {
     let contact: String?
@@ -13,7 +15,27 @@ struct RecruitContactLinkInfo: Codable {
 }
 
 final class RecruitContactLinkInfoViewModel: CommonViewModel {
+    let disposeBag = DisposeBag()
     var jobType: Job?
+    var selectedContactTypeOption = PublishRelay<ContactTypeOptions>()
+    var contactType: ContactTypeOptions?
+    
+    /// 연락방법 바텀시트 노출
+    func showContactTypeBottomSheet() {
+        let optionsBottomSheetViewModel = OptionsBottomSheetViewModel(
+            sceneCoordinator: sceneCoordinator,
+            title: "연락방법",
+            selectedItem: contactType ?? .kakaoOpenChat,
+            list: ContactTypeOptions.allCases
+        ) { [weak self] selectedText in
+            guard let self = self else { return }
+            guard let option = ContactTypeOptions.getType(title: selectedText) else { return }
+            self.selectedContactTypeOption.accept(option)
+            self.sceneCoordinator.close(animated: true)
+        }
+        let scene = Scene.optionsBottomSheet(optionsBottomSheetViewModel)
+        sceneCoordinator.transition(to: scene, using: .customModal, animated: true)
+    }
     
     func moveToNextStep(recruitContactLinkInfo: RecruitContactLinkInfo) {
         let recruitBasicInfoViewModel = RecruitBasicInfoViewModel(sceneCoordinator: sceneCoordinator, jobType: jobType, recruitContactLinkInfo: recruitContactLinkInfo)
