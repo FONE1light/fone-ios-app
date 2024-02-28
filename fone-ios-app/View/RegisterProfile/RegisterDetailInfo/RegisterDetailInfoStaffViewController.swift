5ef53a6173fc2f391b0836660396255c565311ef
나경 <nagyeong.yeo1023@gmail.com>
2024-02-28 16:09:09 +0900

    style: 불필요 코드 제거

--- fone-ios-app/View/RegisterProfile/RegisterDetailInfo/RegisterDetailInfoStaffViewController.swift
+++ fone-ios-app/View/RegisterProfile/RegisterDetailInfo/RegisterDetailInfoStaffViewController.swift
@@ -194,21 +194,7 @@ class RegisterDetailInfoStaffViewController: UIViewController, ViewModelBindable
                 owner.present(popup, animated: false)
                 
             }.disposed(by: rx.disposeBag)
-        
-//        instagramButton.rx.tap
-//            .withUnretained(self)
-//            .bind { owner, _ in
-//                let bottomSheet = SNSBottomSheet(type: .instagram, link: owner.viewModel.instagramLink)
-//                owner.presentPanModal(view: bottomSheet)
-//            }.disposed(by: rx.disposeBag)
-//        
-//        youtubeButton.rx.tap
-//            .withUnretained(self)
-//            .bind { owner, _ in
-//                    let bottomSheet = SNSBottomSheet(type: .youtube, link: owner.viewModel.youtubeLink)
-//                    owner.presentPanModal(view: bottomSheet)
-//            }.disposed(by: rx.disposeBag)
-        
+    
         nextButton.rx.tap
             .withUnretained(self)
             .bind { owner, _ in
