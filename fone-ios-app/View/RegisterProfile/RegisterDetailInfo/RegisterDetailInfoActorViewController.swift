374585aa8d9219f757eff5325932717560592e62
나경 <nagyeong.yeo1023@gmail.com>
2024-02-28 16:04:49 +0900

    feat: 스크롤뷰 및 자동 스크롤 기능 추가

--- fone-ios-app/View/RegisterProfile/RegisterDetailInfo/RegisterDetailInfoActorViewController.swift
+++ fone-ios-app/View/RegisterProfile/RegisterDetailInfo/RegisterDetailInfoActorViewController.swift
@@ -16,6 +16,9 @@ class RegisterDetailInfoActorViewController: UIViewController, ViewModelBindable
     var viewModel: RegisterDetailInfoActorViewModel!
     var disposeBag = DisposeBag()
     
+    private let scrollView = UIScrollView()
+    private let contentView = UIView()
+    
     let stackView = UIStackView().then {
         $0.axis = .vertical
         $0.spacing = 20
@@ -221,6 +224,9 @@ class RegisterDetailInfoActorViewController: UIViewController, ViewModelBindable
         setupUI()
         setConstraints()
         
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name:UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name:UIResponder.keyboardWillHideNotification, object: nil)
+        
     }
     
     private func setNavigationBar() {
@@ -233,15 +239,19 @@ class RegisterDetailInfoActorViewController: UIViewController, ViewModelBindable
     
     private func setupUI() {
         view.backgroundColor = .white_FFFFFF
+        view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
         
         [
             stepIndicator,
             titleLabel,
             stackView,
-            snsBlock,
-            nextButton
+            snsBlock
         ]
-            .forEach { view.addSubview($0) }
+            .forEach { contentView.addSubview($0) }
+        
+        view.addSubview(nextButton)
+        
         
         [
             birthBlock,
@@ -283,8 +293,17 @@ class RegisterDetailInfoActorViewController: UIViewController, ViewModelBindable
     }
     
     private func setConstraints() {
+        scrollView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        contentView.snp.makeConstraints {
+            $0.width.equalToSuperview()
+            $0.edges.equalTo(scrollView.contentLayoutGuide)
+        }
+        
         stepIndicator.snp.makeConstraints {
-            $0.top.equalTo(view.safeAreaLayoutGuide).offset(30)
+            $0.top.equalToSuperview().offset(30)
             $0.leading.equalToSuperview().offset(16)
         }
         
@@ -311,6 +330,7 @@ class RegisterDetailInfoActorViewController: UIViewController, ViewModelBindable
         snsBlock.snp.makeConstraints {
             $0.top.equalTo(stackView.snp.bottom).offset(30)
             $0.leading.trailing.equalTo(stackView)
+            $0.bottom.equalToSuperview()
         }
         
         nextButton.snp.makeConstraints {
@@ -396,3 +416,21 @@ class RegisterDetailInfoActorViewController: UIViewController, ViewModelBindable
     }
 }
 
+extension RegisterDetailInfoActorViewController {
+    @objc func keyboardWillShow(notification: NSNotification) {
+        guard let userInfo = notification.userInfo else { return }
+        var keyboardFrame:CGRect = (userInfo[UIResponder.keyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+        keyboardFrame = self.view.convert(keyboardFrame, from: nil)
+
+        var contentInset = self.scrollView.contentInset
+        contentInset.bottom = keyboardFrame.size.height + 100 // TODO: 상수 삭제
+        scrollView.contentInset = contentInset
+//        scrollView.scrollIndicatorInsets = contentInset
+    }
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+        let contentInset = UIEdgeInsets.zero
+        scrollView.contentInset = contentInset
+    }
+    
+}
