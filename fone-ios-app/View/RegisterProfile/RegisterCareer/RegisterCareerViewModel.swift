393fd6bddd46d6fdb2ce513a5bcc5f911536a2f8
나경 <nagyeong.yeo1023@gmail.com>
2024-02-28 17:01:44 +0900

    feat: 프로필 검증 API - 페이지 5 연결

--- fone-ios-app/View/RegisterProfile/RegisterCareer/RegisterCareerViewModel.swift
+++ fone-ios-app/View/RegisterProfile/RegisterCareer/RegisterCareerViewModel.swift
@@ -5,13 +5,35 @@
 //  Created by 여나경 on 11/27/23.
 //
 
-import Foundation
+import RxSwift
 
 class RegisterCareerViewModel: CommonViewModel {
     
+    private let disposeBag = DisposeBag()
     var jobType: Job?
     
-    func moveToRegisterInterest() {
+    func validate(career: String?, careerDetail: String?) {
+        let careerRequest = CareerRequest(career: career, careerDetail: careerDetail)
+        
+        profileInfoProvider.rx.request(.validateCareer(request: careerRequest))
+            .mapObject(Result<EmptyData>.self)
+            .asObservable()
+            .withUnretained(self)
+            .subscribe(
+                onNext: { owner, response in
+                    if response.result?.isSuccess == true {
+                        owner.moveToRegisterInterest()
+                    } else {
+                        response.message?.toast(positionType: .withButton)
+                    }
+                },
+                onError: { error in
+                    error.showToast(modelType: String.self, positionType: .withButton)
+                }
+            ).disposed(by: disposeBag)
+    }
+    
+    private func moveToRegisterInterest() {
         let sceneCoordinator = sceneCoordinator
         let registerInterestViewModel = RegisterInterestViewModel(sceneCoordinator: sceneCoordinator)
         registerInterestViewModel.jobType = jobType
--- fone-ios-app/View/RegisterProfile/RegisterDetailContent/RegisterDetailContentViewController.swift
+++ fone-ios-app/View/RegisterProfile/RegisterDetailContent/RegisterDetailContentViewController.swift
@@ -57,7 +57,7 @@ class RegisterDetailContentViewController: UIViewController, ViewModelBindableTy
         nextButton.rx.tap
             .withUnretained(self)
             .bind { owner, _ in
-                owner.viewModel.validate(details: owner.letterCountedTextView.textView?.text)
+                owner.viewModel.validate(details: owner.letterCountedTextView.text)
             }.disposed(by: rx.disposeBag)
     }
     
