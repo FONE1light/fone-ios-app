99bbb042ed72cda4291621baa1c6aa635ce67b7f
Yukyung Huh <huhyukyung@gmail.com>
2024-03-02 21:33:09 +0900

    [FO-439][iOS] 인기 영화제 (#165)
    * feat: 영화제 cell
    
    * feat: 인기 영화제 모듈 configure

--- fone-ios-app/View/Home/Modules/Competition/CompetitionModule.swift
+++ fone-ios-app/View/Home/Modules/Competition/CompetitionModule.swift
@@ -16,20 +16,40 @@ class CompetitionModule: UICollectionViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        
+        collectionView.dataSource = self
+        collectionView.register(FilmCompetitionCell.self)
     }
 
     func setModuelInfo(info: CompetitionModuleInfo?) {
-        titleLabel.text = info?.title ?? "인기 영화제"
+        guard let info else {
+            errorView.isHidden = false
+            return
+        }
         self.competitionInfo = info
         setModule()
     }
     
     private func setModule() {
-        guard competitionInfo != nil else {
-            errorView.isHidden = false
-            collectionView.isHidden = true
-            return
-        }
+        errorView.isHidden = true
+        titleLabel.text = competitionInfo?.title ?? "인기 영화제"
         collectionView.reloadData()
     }
 }
+
+extension CompetitionModule: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        let maxCount = 5
+        let itemCount = competitionInfo?.data?.content?.count ?? 0
+        return min(itemCount, maxCount)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(forIndexPath: indexPath) as FilmCompetitionCell
+        if let item = competitionInfo?.data?.content?[indexPath.item] {
+            cell.configure(item: item, index: indexPath.item)
+        }
+        return cell
+    }
+}
+
