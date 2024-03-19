db72e3758909a0a6f923fa6ac9890b92b49226c9
huhyukyung@gmail.com <huhyukyung@gmail.com>
2024-03-19 22:54:33 +0900

    feat: 버전 변경, 스플래시 3초 삭제

--- fone-ios-app/AppDelegate.swift
+++ fone-ios-app/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        sleep(3)
+//        sleep(3)
         
         let coordinator = SceneCoordinator(window: window!)
         
