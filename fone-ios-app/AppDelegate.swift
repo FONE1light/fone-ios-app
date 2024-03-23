c26d3332aa838e0ba699900bf09c8ec4759d2485
Aiden <84381320+g0getter@users.noreply.github.com>
2024-03-24 03:56:55 +0900

    [FO-988] 강업 기능 (#210)
    * feat: RemoteConfig 추가, 관련 함수 실행
    
    * feat: FakeLaunchScreenViewController, 업데이트 알럿 추가
    
    * feat: UIAlertController 익스텐션 파라미터 수정
    
    * feat: AppStore 연결
    
    * feat: RemoteConfig minimumFetchInterval 1시간으로 설정(AOS와 동일)
    
    * fix: logoFilmOne contentMode 통일

--- fone-ios-app/AppDelegate.swift
+++ fone-ios-app/AppDelegate.swift
@@ -19,19 +19,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         // MARK: 소셜로그인 초기화 (kakao)
         SocialLoginManager.shared.initailize(sceneCoordinator: coordinator)
-        
-        var destinationScene: Scene
-        let accessToken = Tokens.shared.accessToken.value
-        if !accessToken.isEmpty {
-            // 사용 중 토큰 갱신 상황을 최소화하기 위해 자동 로그인 시 토큰 갱신
-            UserManager.shared.reissueToken { _ in }
-            destinationScene = Scene.home(coordinator)
-        } else {
-            let loginViewModel = LoginViewModel(sceneCoordinator: coordinator)
-            destinationScene = Scene.login(loginViewModel)
+        VersionChecker.shared.initialize(sceneCoordinator: coordinator) { isSuccess in
+            
+            DispatchQueue.main.async {
+                var destinationScene: Scene
+                
+                if isSuccess {
+                    destinationScene = self.checkLoginStatus(coordinator)
+                } else {
+                    destinationScene = Scene.fakeLaunchScreen(coordinator)
+                }
+                coordinator.transition(to: destinationScene, using: .root, animated: false)
+            }
         }
         
-        coordinator.transition(to: destinationScene, using: .root, animated: false)
         sleep(3)
         
         return true
@@ -46,3 +47,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 }
 
+
+// MARK: - 화면 이동
+extension AppDelegate {
+    private func checkLoginStatus(_ coordinator: SceneCoordinator) -> Scene {
+        var destinationScene: Scene
+        let accessToken = Tokens.shared.accessToken.value
+        if !accessToken.isEmpty {
+            // 사용 중 토큰 갱신 상황을 최소화하기 위해 자동 로그인 시 토큰 갱신
+            UserManager.shared.reissueToken { _ in }
+            destinationScene = Scene.home(coordinator)
+        } else {
+            let loginViewModel = LoginViewModel(sceneCoordinator: coordinator)
+            destinationScene = Scene.login(loginViewModel)
+        }
+        
+        return destinationScene
+    }
+}
