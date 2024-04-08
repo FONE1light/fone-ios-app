c748022ed402ba86648cc72b357901753cc903f7
huhyukyung@gmail.com <huhyukyung@gmail.com>
2024-04-08 22:48:16 +0900

    feat: add ReportViewController, ReportViewModel

---
+++ fone-ios-app/View/Report/ReportViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ReportViewController.swift
+//  fone-ios-app
+//
+//  Created by Yukyung Huh on 4/8/24.
+//
+
+import UIKit
+
+class ReportViewController: UIViewController, ViewModelBindableType {
+    var viewModel: ReportViewModel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    func bindViewModel() {
+        
+    }
+}
---
+++ fone-ios-app/View/Report/ReportViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ReportViewModel.swift
+//  fone-ios-app
+//
+//  Created by Yukyung Huh on 4/8/24.
+//
+
+import Foundation
+
+final class ReportViewModel: CommonViewModel {
+    
+}
