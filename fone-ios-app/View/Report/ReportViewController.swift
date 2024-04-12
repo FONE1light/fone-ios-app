b5f101af8d35bc52a21b4154d41d7218b517e725
huhyukyung@gmail.com <huhyukyung@gmail.com>
2024-04-13 00:23:32 +0900

    feat: 프로필, 모집글에 따른 달라지는 불편사항 뷰 처리

--- fone-ios-app/View/Report/ReportViewController.swift
+++ fone-ios-app/View/Report/ReportViewController.swift
@@ -15,6 +15,10 @@ class ReportViewController: UIViewController, ViewModelBindableType {
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var nicknameLabel: UILabel!
     @IBOutlet weak var userJobLabel: UILabel!
+    @IBOutlet weak var profileReportButton: UIButton!
+    @IBOutlet weak var jobOpeningReportButton: UIButton!
+    @IBOutlet weak var profileInconveniences: UIView!
+    @IBOutlet weak var jobOpeningInconveniences: UIView!
     @IBOutlet weak var letterCountedTextView: LetterCountedTextView!
     @IBOutlet weak var submitButton: UIButton!
     
@@ -27,6 +31,24 @@ class ReportViewController: UIViewController, ViewModelBindableType {
     func bindViewModel() {
         setUserInfo()
         
+        profileReportButton.rx.tap
+            .withUnretained(self)
+            .bind { owner, _ in
+                owner.setActivated(button: owner.profileReportButton, activated: true)
+                owner.setActivated(button: owner.jobOpeningReportButton, activated: false)
+                owner.profileInconveniences.isHidden = false
+                owner.jobOpeningInconveniences.isHidden = true
+            }.disposed(by: rx.disposeBag)
+        
+        jobOpeningReportButton.rx.tap
+            .withUnretained(self)
+            .bind { owner, _ in
+                owner.setActivated(button: owner.profileReportButton, activated: false)
+                owner.setActivated(button: owner.jobOpeningReportButton, activated: true)
+                owner.profileInconveniences.isHidden = true
+                owner.jobOpeningInconveniences.isHidden = false
+            }.disposed(by: rx.disposeBag)
+        
         closeButton.rx.tap
             .withUnretained(self)
             .bind { owner, _ in
@@ -36,12 +58,17 @@ class ReportViewController: UIViewController, ViewModelBindableType {
     
     private func setUI() {
         letterCountedTextView.xibInit(placeholder: placeholder, textViewHeight: 92, maximumLetterCount: 500)
-        submitButton.applyShadow(shadowType: .shadowBt)
     }
     
     private func setUserInfo() {
         profileImageView.kf.setImage(with: URL(string: viewModel.profileImageURL ?? ""))
         nicknameLabel.text = viewModel.nickname
         userJobLabel.text = viewModel.userJob
+    }
+    
+    private func setActivated(button: UIButton, activated: Bool) {
+        let color = activated ? UIColor.red_F43663 : UIColor.gray_D9D9D9
+        button.borderColor = color
+        button.setTitleColor(color, for: .normal)
     }
 }
